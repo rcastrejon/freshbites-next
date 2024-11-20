bd33946c6b97dcd5557f7ac20b4fe51b484136ef
Rodrigo Castrejon <me@rcastrejon.dev>
2024-11-20 14:29:24 -0600

    update seed script for improved local development

--- src/lib/db/seed.ts
+++ src/lib/db/seed.ts
@@ -1,12 +1,40 @@
 import "../server/envConfig";
 
 import { db } from ".";
-import { recipeTable } from "./schema";
+import { recipeTable, userTable } from "./schema";
 import recipes from "./recipes.json";
 import { dropAll, upsertMultipleVectors } from "../server/vectors";
+import type { AuthorInsert } from "./types";
 
 async function main() {
   await db.delete(recipeTable).all();
+  await db.delete(userTable).all();
+
+  const usersRes = await fetch("https://api.clerk.com/v1/users", {
+    headers: {
+      Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+    },
+  });
+
+  if (!usersRes.ok) {
+    throw new Error("Failed to fetch users");
+  }
+
+  const users = (await usersRes.json()) as Record<string, unknown>[];
+  const userValues = users.map(
+    (user) =>
+      ({
+        id: user.id,
+        username: user.username,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        imageUrl: user.image_url,
+        createdAt: user.created_at,
+      }) as AuthorInsert,
+  );
+
+  await db.insert(userTable).values(userValues);
+  console.log("Seeded users");
 
   const transformedRecipes = recipes.map((recipe) => ({
     ...recipe,
--- src/lib/db/types.ts
+++ src/lib/db/types.ts
@@ -4,3 +4,5 @@ export type Recipe = typeof recipeTable.$inferSelect;
 export type Author = typeof userTable.$inferSelect;
 
 export type RecipeWithAuthor = Recipe & { author: Author | null };
+
+export type AuthorInsert = typeof userTable.$inferInsert;
